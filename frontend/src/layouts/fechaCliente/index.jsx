3b96a280012d7139011c8f6dc24d512e05fbeeab
LucasBoldriniPSM <lboldrini.emapsm@gmail.com>
2024-10-16 10:47:36 -0300

    adapto a mobile

--- frontend/src/layouts/fechaCliente/index.jsx
+++ frontend/src/layouts/fechaCliente/index.jsx
@@ -66,33 +66,33 @@ const FechaCliente = () => {
       setDatosFiltrados([]);
       return;
     }
-  
+
     try {
       // Primera solicitud: fecha-cliente
       const url1 = new URL(`${API_BACK}/api/fecha-cliente`);
       const params1 = {
-        cliente: cliente || '',
-        grupoCliente: user.idGrupoCliente || '',
-        fechaDesde: fechaDesde || '',
-        fechaHasta: fechaHasta || '',
+        cliente: cliente || "",
+        grupoCliente: user.idGrupoCliente || "",
+        fechaDesde: fechaDesde || "",
+        fechaHasta: fechaHasta || "",
       };
-  
+
       Object.keys(params1).forEach((key) => {
         if (params1[key]) {
           url1.searchParams.append(key, params1[key]);
         }
       });
-  
+
       const response1 = await fetch(url1);
-      console.log("Response",response1.status)
+      console.log("Response", response1.status);
       const apiData1 = await response1.json();
-  
+
       if (apiData1.dataTabla) {
         setAllData(apiData1.dataTabla);
         setColumns(apiData1.columns);
         filtrarDatos(apiData1.dataTabla, cliente, fechaDesde, fechaHasta);
       } else if (response1.status === 404) {
-        cambiarEstadoPopUp1(true)
+        cambiarEstadoPopUp1(true);
         console.error("No se recibieron datos de fecha-cliente API");
         setAllData([]);
       }
@@ -100,7 +100,7 @@ const FechaCliente = () => {
       console.error("Error en la solicitud fecha-cliente:", error);
       setAllData([]);
     }
-  
+
     try {
       // Segunda solicitud: geoMapaItems
       const url2 = new URL(`${API_BACK}/apiFecha/geoMapaItems`);
@@ -110,16 +110,16 @@ const FechaCliente = () => {
         fechaDesde: fechaDesde,
         fechaHasta: fechaHasta,
       };
-  
+
       Object.keys(params2).forEach((key) => {
         if (params2[key]) {
           url2.searchParams.append(key, params2[key]);
         }
       });
-  
+
       const response2 = await fetch(url2);
       const apiData2 = await response2.json();
-  
+
       if (apiData2.dataTabla) {
         setPuntosMapa(apiData2.dataTabla);
         setColumnsMapa(apiData2.columns);
@@ -132,7 +132,7 @@ const FechaCliente = () => {
       setPuntosMapa([]);
     }
   };
-  
+
   const convertirFecha = (fechaStr) => {
     const partes = fechaStr.split("/");
     if (partes.length === 3) {
@@ -143,13 +143,8 @@ const FechaCliente = () => {
     return new Date(); // Retorna una fecha inválida si el formato no es el esperado
   };
 
-  const filtrarDatos = (
-    data,
-    cliente,
-    fechaDesde,
-    fechaHasta
-  ) => {
-    console.log("DAAA", data)
+  const filtrarDatos = (data, cliente, fechaDesde, fechaHasta) => {
+    console.log("DAAA", data);
     const datosFiltrados = data.filter((item) => {
       console.log("WASA1", fechaDesde);
       console.log("WASA2", fechaHasta);
@@ -162,7 +157,7 @@ const FechaCliente = () => {
       const itemFecha = `${año}-${mes}-${dia}`; // Reorganiza a 'yyyy-mm-dd'// Formato YYYY-MM-DD
       console.log("WASA33", itemFecha);
 
-      console.log("item", item)
+      console.log("item", item);
       const cumpleCliente = cliente ? item.nroCliente === cliente : true;
       const cumpleFechaDesde = fechaDesde ? itemFecha >= fechaDesde : true;
       const cumpleFechaHasta = fechaHasta ? itemFecha <= fechaHasta : true;
@@ -171,11 +166,7 @@ const FechaCliente = () => {
       console.log("WASA2", fechaHasta);
       console.log("WASA3", itemFecha);
 
-      return (
-        cumpleCliente &&
-        cumpleFechaDesde &&
-        cumpleFechaHasta
-      );
+      return cumpleCliente && cumpleFechaDesde && cumpleFechaHasta;
     });
 
     console.log("dataaa", datosFiltrados);
@@ -189,7 +180,7 @@ const FechaCliente = () => {
     let arrayCoordenadas = [];
 
     for (let i = 0; i < data.length; i++) {
-      if (data[i].latitud && data[i].longitud){
+      if (data[i].latitud && data[i].longitud) {
         // Genera valores aleatorios de longitud y latitud
         const latitud = parseFloat(data[i].latitud);
         const longitud = parseFloat(data[i].longitud);
@@ -203,150 +194,182 @@ const FechaCliente = () => {
 
   return (
     <>
-    <SoftBox display="flex" flexDirection="column" alignItems="center">
-      <SoftBox width="100%">
-        <ResponsiveAppBar />
-      </SoftBox>
+      <SoftBox display="flex" flexDirection="column" alignItems="center">
+        <SoftBox width="100%">
+          <ResponsiveAppBar />
+        </SoftBox>
 
-      <Card style={{ marginTop: "7rem", width: "90%" }}>
-        <SoftBox p={3}>
-          <SoftTypography variant="h4">Filtros</SoftTypography>
-          <Divider />
+        <Card style={{ marginTop: "7rem", width: "90%" }}>
+          <SoftBox p={3}>
+            <SoftTypography variant="h4">Filtros</SoftTypography>
+            <Divider />
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <SoftBox
-              display="flex"
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <SoftBox>
-                <SoftTypography marginTop={-2}>N° de Cliente</SoftTypography>
-                <Controller
-                  name="numCliente"
-                  control={control}
-                  rules={{ required: "Campo obligatorio" }}
-                  render={({ field }) => (
-                    <>
-                      <SoftInputBase
-                        field={field}
-                        placeholder="Inserte nro de cliente"
-                        error={!!errors.numCliente} // Muestra borde rojo si hay error
-                      />
-                      {errors.numCliente && (
-                        <SoftTypography
-                          color="error"
-                          fontSize="1rem"
-                          marginTop={1}
-                        >
-                          {errors.numCliente.message}
-                        </SoftTypography>
-                      )}
-                    </>
-                  )}
-                />
-              </SoftBox>
-
-              <SoftBox display="flex" flexDirection="column" marginTop={-2}>
-                <SoftTypography marginBottom={-1}>Fecha</SoftTypography>
-                <SoftBox display="flex" alignItems="center">
-                  <Controller
-                    name="fechaDesde"
-                    control={control}
-                    render={({ field }) => <DatePickerValue field={field} />}
-                  />
-                  <SoftTypography> - </SoftTypography>
-                  <Controller
-                    name="fechaHasta"
-                    control={control}
-                    render={({ field }) => <DatePickerValue field={field} />}
-                  />
-                </SoftBox>
-              </SoftBox>
-
+            <form onSubmit={handleSubmit(onSubmit)}>
               <SoftBox
                 display="flex"
-                justifyContent="flex-end"
+                justifyContent="space-between"
                 alignItems="center"
-                pt={3}
-                px={3}
+                flexDirection={{ xs: "column", md: "row" }} // Responsive layout
               >
-                <SoftButton variant="gradient" color="info">
-                  <input
-                    type="submit"
-                    value="Filtrar"
-                    style={{
-                      background: "transparent",
-                      border: "none",
-                      fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
-                      fontSize: "0.875rem",
-                      fontWeight: "700",
-                      color: "#FFFFFF",
-                      textTransform: "uppercase",
-                    }}
+                <SoftBox>
+                  <SoftTypography
+                    marginTop={-2}
+                    fontSize={{ xs: "1rem", sm: "1.25rem" }} // Responsive font size
+                  >
+                    N° de Cliente
+                  </SoftTypography>
+                  <Controller
+                    name="numCliente"
+                    control={control}
+                    rules={{ required: "Campo obligatorio" }}
+                    render={({ field }) => (
+                      <>
+                        <SoftInputBase
+                          field={field}
+                          placeholder="Inserte nro de cliente"
+                          error={!!errors.numCliente} // Muestra borde rojo si hay error
+                        />
+                        {errors.numCliente && (
+                          <SoftTypography
+                            color="error"
+                            fontSize="1rem"
+                            marginTop={1}
+                          >
+                            {errors.numCliente.message}
+                          </SoftTypography>
+                        )}
+                      </>
+                    )}
                   />
-                </SoftButton>
-              </SoftBox>
-            </SoftBox>
-          </form>
-        </SoftBox>
-      </Card>
+                </SoftBox>
 
-      <SoftBox py={3} style={{ width: "90%" }} justifyContent="center">
-        <SoftBox justifyContent="center">
-          <Card>
-            {user ? (
-              <SoftBox p={3}>
-                {(user.idGrupoCliente === 2 || user.idGrupoCliente === null) && puntosMapa ? (
-                  <MyMap arrayPuntos={armarArrayCoordenadas(puntosMapa)} />
-                ) : null}
+                <SoftBox
+                  display="flex"
+                  flexDirection="column"
+                  marginTop={{ xs: 2, md: -2 }}
+                  marginLeft={{ md: 3 }}
+                >
+                  <SoftTypography
+                    marginBottom={-1}
+                    fontSize={{ xs: "1rem", sm: "1.25rem" }}
+                  >
+                    Fecha
+                  </SoftTypography>
+                  <SoftBox display="flex" alignItems="center" flexDirection={{ xs: "column", md: "row" }}>
+                    <Controller
+                      name="fechaDesde"
+                      control={control}
+                      render={({ field }) => <DatePickerValue field={field} />}
+                    />
+                    <SoftTypography> - </SoftTypography>
+                    <Controller
+                      name="fechaHasta"
+                      control={control}
+                      render={({ field }) => <DatePickerValue field={field} />}
+                    />
+                  </SoftBox>
+                </SoftBox>
+
+                <SoftBox
+                  display="flex"
+                  justifyContent="flex-end"
+                  alignItems="center"
+                  pt={3}
+                  px={3}
+                >
+                  <SoftButton variant="gradient" color="info">
+                    <input
+                      type="submit"
+                      value="Filtrar"
+                      style={{
+                        background: "transparent",
+                        border: "none",
+                        fontFamily: '"Roboto","Helvetica","Arial",sans-serif',
+                        fontSize: "0.875rem",
+                        fontWeight: "700",
+                        color: "#FFFFFF",
+                        textTransform: "uppercase",
+                      }}
+                    />
+                  </SoftButton>
+                </SoftBox>
               </SoftBox>
-            ) : null}
-          </Card>
+            </form>
+          </SoftBox>
+        </Card>
+
+        <SoftBox py={3} style={{ width: "90%" }} justifyContent="center">
+          <SoftBox justifyContent="center">
+            <Card>
+              {user ? (
+                <SoftBox p={3}>
+                  {(user.idGrupoCliente === 2 ||
+                    user.idGrupoCliente === null) &&
+                  puntosMapa ? (
+                    <MyMap arrayPuntos={armarArrayCoordenadas(puntosMapa)} />
+                  ) : null}
+                </SoftBox>
+              ) : null}
+            </Card>
+          </SoftBox>
+        </SoftBox>
+
+        <SoftBox
+          paddingBottom={3}
+          style={{ width: "90%" }}
+          justifyContent="center"
+        >
+          <SoftBox justifyContent="center">
+            <Card>
+              {user ? (
+                <SoftBox p={3}>
+                  {user.idGrupoCliente !== 4 ? (
+                    <PRSTable data={datosFiltrados} columns={columns} />
+                  ) : (
+                    <CalleAlturaTable data={datosFiltrados} columns={columns} />
+                  )}
+                </SoftBox>
+              ) : null}
+            </Card>
+          </SoftBox>
         </SoftBox>
       </SoftBox>
 
-      <SoftBox paddingBottom={3} style={{ width: "90%" }} justifyContent="center">
-        <SoftBox justifyContent="center">
-          <Card>
-            {user ? (
-              <SoftBox p={3}>
-                {user.idGrupoCliente !== 4 ? (
-                  <PRSTable data={datosFiltrados} columns={columns} />
-                ) : (
-                  <CalleAlturaTable data={datosFiltrados} columns={columns} />
-                )}
-              </SoftBox>
-            ) : null}
-          </Card>
-        </SoftBox>
-      </SoftBox>
-    </SoftBox>
-
-    <PopUp
-      estado={estadoPopUp1}
-      cambiarEstado={cambiarEstadoPopUp1}
-      titulo=""
-      mostrarHeader={true}
-      mostrarOverlay={true}
-      posicionModal={"center"}
-      padding={"0px"}
-      width={"30vw"}
-      height={"15vh"}
-      background={"#085397"}
-      paddingTopEncabezado={'20px'}
-    >
-      <Contenido>
-        {/* Contenido del PopUp */}
-        <SoftBox display="flex" justifyContent="center" align-items="center">
-          <SoftTypography varint="button" fontWeight="medium" color="dark" px={3}
-              py={2} style={{fontSize: '1rem', display:'flex', justifyContent:'center', alignItems:'center'}}>
-            No encotramos informacion para este cliente.
-          </SoftTypography>
-        </SoftBox>
-      </Contenido>
-    </PopUp>
-
-  </>
+      <PopUp
+        estado={estadoPopUp1}
+        cambiarEstado={cambiarEstadoPopUp1}
+        titulo=""
+        mostrarHeader={true}
+        mostrarOverlay={true}
+        posicionModal={"center"}
+        padding={"0px"}
+        width={"30vw"}
+        height={"15vh"}
+        background={"#085397"}
+        paddingTopEncabezado={"20px"}
+      >
+        <Contenido>
+          {/* Contenido del PopUp */}
+          <SoftBox display="flex" justifyContent="center" align-items="center">
+            <SoftTypography
+              varint="button"
+              fontWeight="medium"
+              color="dark"
+              px={3}
+              py={2}
+              style={{
+                fontSize: "1rem",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              No encotramos informacion para este cliente.
+            </SoftTypography>
+          </SoftBox>
+        </Contenido>
+      </PopUp>
+    </>
   );
 };
 
